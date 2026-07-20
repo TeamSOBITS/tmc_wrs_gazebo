48157eec99bfc50f8d24ad95736d4d10bb344c14
kurokara-YK <kurokara1226@gmail.com>
2026-07-20 16:35:34 +0900

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyr
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y \
+    ros-jazzy-xacro \
     gz-harmonic
 
 
